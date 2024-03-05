dd140abcbb5f8a0f0ff4876a393cf1baccefeeea
jayrajvmu <jayarajvmu@gmail.com>
2024-03-05 21:22:41 +0530

    add

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,223 +1,260 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import "../App.css";
 import "./Products.css";
 
 
-export default function Product(){
-    let [productsData, setproductsData] = useState();
-    let category=[];
+export default function Product() {
+  let [productsData, setproductsData] = useState();
+  let category = [];
 
-    let [categorytype, setCategorytype]=useState();
+  let [categorytype, setCategorytype] = useState();
 
-    useEffect(() => {
-       let getAPI = () => {
-         fetch('https://dummyjson.com/products?limit=100')
-           .then((response) => response.json())
-           .then((json) => {
-             setproductsData(json);
-           });
-       };
-       getAPI();
-
-     }, []);
-     productsData != undefined &&productsData.products.forEach(element => {
-   if(category.indexOf(element.category) === -1){
-    category.push(element.category);
-   }  
-    });
+  let [acendingsort, setAcendingsort ]=useState(1);
+  let [decendingsort, setDecendingsort ]=useState(1);
 
 
-     let changeCategory = (e)=>{
+  useEffect(() => {
+    let getAPI = () => {
+      fetch('https://dummyjson.com/products?limit=100')
+        .then((response) => response.json())
+        .then((json) => {
+          setproductsData(json);
+        });
+    };
+    getAPI();
 
-      // getElementsbyClassName('categorydata')
+  }, []);
+  productsData != undefined && productsData.products.forEach(element => {
+    if (category.indexOf(element.category) === -1) {
+      category.push(element.category);
+    }
+  });
 
-      let categoryTotalData=document.getElementsByClassName('categorydata');
 
-      // console.log(categoryTotalData);
+  let changeCategory = (e) => {
 
-      // categoryTotalData.forEach(element=>{
-      //   console.log(element);
-      // })
+    let categoryTotalData = document.getElementsByClassName('categorydata');
 
-      for (let data of categoryTotalData) {
 
-        data.style.color="black";
-       
+    for (let data of categoryTotalData) {
+
+      data.style.color = "black";
+
     }
 
 
 
-      setCategorytype(e.target.id);
+    setCategorytype(e.target.id);
 
-      e.target.style.color="blue";
-     }
-
-    return(
-
-        <div>
-           <div className='container-fluid content'>
-            <div className='row'>
-                <div className='col-2'>
-
-                    <h2 className='poppins-semibold f-20'>Category</h2>
-
-                    <div>
-                    {category != undefined && category.map((product, index) => {
-
-                        return(
-
-                            <div key={index} className='poppins-light f-14 categorydata' onClick={changeCategory} id={product}>
- 
-                             
-                                {product}
-                            
-                                </div>
-                        )
-                    })}
+    e.target.style.color = "blue";
+  }
 
 
+  let defaultsort=()=>{
+    setAcendingsort(1);
+    setDecendingsort(1);
+  }
 
+  let acendingsorting=()=>{
 
-                    </div>
-
-                </div>
-
-                <div className='col-10'>
-
-                  <div className='brudcrum d-flex d-flex d-flex justify-content-between'>
-
-                    <div>
-
-                    <span className='f-20 poppins-regular'>Products</span> &gt; <span className='f-20 poppins-semibold'>{categorytype?categorytype:"all"}</span>
-                  
-
-                    </div>
-
-                    <div>
-
-                      right
-
-                    </div>
-
-                  </div>
-
-                  
-                 <div className='card-container'>
-
-
-
-
-
-{
-
-categorytype ? productsData != undefined && productsData.products.map((product, index) => {
-  
-if(categorytype==product.category){
-    return (
-
-    <div className="card"  key={product.id}>
-
-            <div className='img-container'><img src={product.images[0]} className="card-img-top product-image" alt="..."/></div>
-         <div className="card-body">
-
-        <div className="card-title poppins-light f-14">{product.category}</div>
-          <div className="card-title poppins-bold f-20">{product.title}</div>
-
-        <div className='product-card-flex'>
-
-            <div className='left'> <div className="card-title poppins-light f-16"><span className="badge rounded-pill bg-danger">{product.rating}</span> </div>
-
-<div className="card-title poppins-light f-14"><del >${product.price}</del> </div>
-
-
-
-  </div>
-
-
-            <div className='right'>
-            <a href="#" className="btn btn-warning my-2">Add to Cart</a>
-                 
-                 
-                 </div>
-
-        </div>
-
-        <div className='product-card-flex'>
-<div className="card-title poppins-light f-16 text-primary"><span className='anta-regular'>${Math.round(product.price-((product.discountPercentage/100)*product.price))}</span> </div>
-
-
-<div className="card-title poppins-light f-16 "><span className='anta-regular'>{product.discountPercentage}% Discount</span> </div>
- </div>
-         
-
-          
-        </div>
-      </div>
-    );
+    setAcendingsort(1);
+    setDecendingsort(-1);
 
   }
 
-  })
+  let decendingsorting=()=>{
+    setAcendingsort(-1);
+    setDecendingsort(1);
+  }
+
+  return (
+
+    <div>
+      <div className='container-fluid content'>
+        <div className='row'>
+          <div className='col-2'>
+
+            <h2 className='poppins-semibold f-20'>Category</h2>
+
+            <div>
+              {category != undefined && category.map((product, index) => {
+
+                return (
+
+                  <div key={index} className='poppins-light f-14 categorydata' onClick={changeCategory} id={product}>
 
 
+                    {product}
 
+                  </div>
+                )
+              })}
 
-
-: productsData != undefined && productsData.products.map((product, index) => {
-
-    return (
-        <div className="card"  key={product.id}>
-
-            <div className='img-container'><img src={product.images[0]} className="card-img-top product-image" alt="..."/></div>
-        <div className="card-body">
-
-        <div className="card-title poppins-light f-14">{product.category}</div>
-          <div className="card-title poppins-bold f-20">{product.title}</div>
-
-        <div className='product-card-flex'>
-
-            <div className='left'> <div className="card-title poppins-light f-16"><span className="badge rounded-pill bg-danger">{product.rating}</span> </div>
-
-<div className="card-title poppins-light f-14"><del >${product.price}</del> </div>
-
-
-
-  </div>
-
-
-            <div className='right'>
-            <a href="#" className="btn btn-warning my-2">Add to Cart</a>
-                 
-                 
-                 </div>
-
-        </div>
-
-        <div className='product-card-flex'>
-<div className="card-title poppins-light f-16 text-primary"><span className='anta-regular'>${Math.round(product.price-((product.discountPercentage/100)*product.price))}</span> </div>
-
-
-<div className="card-title poppins-light f-16 "><span className='anta-regular'>{product.discountPercentage}% Discount</span> </div>
- </div>
-         
-
-          
-        </div>
-      </div>
-    );
-  })}
-
-
-
-                 </div>
-
-                </div>
 
 
 
             </div>
-           </div>
+
+          </div>
+
+          <div className='col-10'>
+
+            <div className='brudcrum d-flex d-flex d-flex justify-content-between'>
+
+              <div>
+
+                <span className='f-20 poppins-regular'>Products</span> &gt; <span className='f-20 poppins-semibold'>{categorytype ? categorytype : "all"}</span>
+
+
+              </div>
+
+              <div>
+
+
+
+<div className="dropdown">
+  <a className="btn btn-secondary dropdown-toggle"  role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown link
+  </a>
+
+  <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a className="dropdown-item"onClick={defaultsort}>Default</a></li>
+    <li><a className="dropdown-item" onClick={acendingsorting}>Price (Low to High)</a></li>
+    <li><a className="dropdown-item" onClick={decendingsorting} >Price (High to Low)</a></li>
+  </ul>
+</div>
+
+              </div>
+
+            </div>
+
+
+            <div className='card-container'>
+
+
+
+
+
+              {
+
+                categorytype ? productsData != undefined && 
+                
+                
+                
+               (
+                
+                productsData.products.sort((a, b) => a.price > b.price? acendingsort:decendingsort).map((product, index) => {
+
+                  if (categorytype == product.category) {
+                    return (
+
+                      <div className="card" key={product.id}>
+
+                        <div className='img-container'><img src={product.images[0]} className="card-img-top product-image" alt="..." /></div>
+                        <div className="card-body">
+
+                          <div className="card-title poppins-light f-14">{product.category}</div>
+                          <div className="card-title poppins-bold f-20">{product.title}</div>
+
+                          <div className='product-card-flex'>
+
+                            <div className='left'> <div className="card-title poppins-light f-16"><span className="badge rounded-pill bg-danger">{product.rating}</span> </div>
+
+                              <div className="card-title poppins-light f-14"><del >${product.price}</del> </div>
+
+
+
+                            </div>
+
+
+                            <div className='right'>
+                              <a href="#" className="btn btn-warning my-2">Add to Cart</a>
+
+
+                            </div>
+
+                          </div>
+
+                          <div className='product-card-flex'>
+                            <div className="card-title poppins-light f-16 text-primary"><span className='anta-regular'>${Math.round(product.price - ((product.discountPercentage / 100) * product.price))}</span> </div>
+
+
+                            <div className="card-title poppins-light f-16 "><span className='anta-regular'>{product.discountPercentage}% Discount</span> </div>
+                          </div>
+
+
+
+                        </div>
+                      </div>
+                    );
+
+                  }
+
+                })
+
+
+
+
+  )
+                
+                
+                : productsData != undefined && productsData.products.map((product, index) => {
+
+                
+
+                    return (
+                      <div className="card" key={product.id}>
+
+                        <div className='img-container'><img src={product.images[0]} className="card-img-top product-image" alt="..." /></div>
+                        <div className="card-body">
+
+                          <div className="card-title poppins-light f-14">{product.category}</div>
+                          <div className="card-title poppins-bold f-20">{product.title}</div>
+
+                          <div className='product-card-flex'>
+
+                            <div className='left'> <div className="card-title poppins-light f-16"><span className="badge rounded-pill bg-danger">{product.rating}</span> </div>
+
+                              <div className="card-title poppins-light f-14"><del >${product.price}</del> </div>
+
+
+
+                            </div>
+
+
+                            <div className='right'>
+                              <a href="#" className="btn btn-warning my-2">Add to Cart</a>
+
+
+                            </div>
+
+                          </div>
+
+                          <div className='product-card-flex'>
+                            <div className="card-title poppins-light f-16 text-primary"><span className='anta-regular'>${Math.round(product.price - ((product.discountPercentage / 100) * product.price))}</span> </div>
+
+
+                            <div className="card-title poppins-light f-16 "><span className='anta-regular'>{product.discountPercentage}% Discount</span> </div>
+                          </div>
+
+
+
+                        </div>
+                      </div>
+                    );
+                  })}
+
+
+
+            </div>
+
+          </div>
+
+
+
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
