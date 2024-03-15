8840bec754764f7b63864081723319e13c3712c9
jayrajvmu <jayarajvmu@gmail.com>
2024-03-15 20:14:24 +0530

    update preview

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -6,14 +6,77 @@ import "./Product.css";
 
 export default function Product(){
 
-    const { id } = useParams();
+    const id = useParams();
 
+    const [product, setproduct] = useState();
 
+    useEffect(() => {
+        let getAPI = () => {
+          fetch('https://dummyjson.com/products/'+id.id)
+            .then((response) => response.json())
+            .then((json) => {
+                setproduct(json);
+    
+    console.log(json);
+    
+            });
+    
+    
+        };
+        getAPI();
+    
+      }, []);
 
     return(
         <div className='top-margin'>
 
-ID: {id}
+
+
+            <div className='container-fluid'>
+
+                <div className='row'>
+
+                    <div className='col-xl-4 col-md-6'>
+
+                        {
+                            product !=undefined &&
+
+                            <img src={product.thumbnail} className="card-img-top product-image" alt="..." />
+
+                        }
+
+
+                    </div>
+                    <div className='col-xl-4 col-md-6'>
+                    {
+                            product !=undefined &&
+
+                            <img src={product.thumbnail} className="card-img-top product-image" alt="..." />
+
+                        }
+                    </div>
+
+                    <div className='col-xl-4 col-md-6'>
+                    {
+                            product !=undefined &&
+
+                            <img src={product.thumbnail} className="card-img-top product-image" alt="..." />
+
+                        }
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+            
+
+
+
+
         </div>
     )
 }
